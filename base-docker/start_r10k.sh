abea7bf663939ee5ea72a3ec853207bf645696eb
Pokhilenko Andrei <Pokhilenko.An@khortitsa.com>
2024-11-19 17:17:58 +0200

    Create develop file

--- base-docker/start_r10k.sh
+++ base-docker/start_r10k.sh
@@ -2,7 +2,8 @@
 # Синхронизация с репозтория Git в локальный репозиторий Pappet
 # Code_Dir - путь к локальному репозиторию.
 
-Code_Dir='./Work'
+
+Code_Dir='./oss/puppet-code'
 Local_git='./r10k_home/code/environments'
 
 [ -d ./r10k_home ] || (mkdir ./r10k_home && chmod 0770 ./r10k_home && chown root:docker ./r10k_home)
@@ -10,12 +11,13 @@ Local_git='./r10k_home/code/environments'
 [ -f ./r10k_home/r10k.yaml ] || cat <<EOF >./r10k_home/r10k.yaml
 :cachedir: '/home/puppet/cache'
 :sources:
- :production:
+ :panda:
   remote: 'https://github.com/P2Andy/puppet-test.git'
   basedir: '/home/puppet/code/environments'
+  ignore_branch_prefixes: ["master", "develop"]
 #
 EOF
 
 docker run -it -v ./r10k_home:/home/puppet --rm puppet/r10k deploy environment
 
-rsync -Pav --exclude=".*" --no-perms --no-owner --no-group --delete-after $Local_git $Code_Dir
+rsync -Paq --exclude=".*" --no-perms --no-owner --no-group --delete-after $Local_git $Code_Dir
